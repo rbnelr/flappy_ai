2fb10f38834cf85cdc1c180bda1699760c3764c8
Anon Ymous <rbnelr@gmx.de>
2019-03-20 20:43:12 +0100

    seperated players and game, implemented random jumping "AI" for testing

--- flappy_ai/flappy_ai.py
+++ flappy_ai/flappy_ai.py
@@ -36,7 +36,7 @@ def load_sprite(name, relative_scale=1, flip_y=False, origin=(0,0)):
 
 	return sprite
 
-def draw_sprite(sprite, pos_screen_px, ori=0): # rotation_axis position inside of sprite in sprite pixels
+def draw_sprite(sprite, pos_screen_px, ori=0, alpha=255): # rotation_axis position inside of sprite in sprite pixels
 	pos = pos_screen_px
 	
 	pos = (pos[0] * sprite_scale, pos[1] * sprite_scale) # place in pixel based global space
@@ -50,6 +50,9 @@ def draw_sprite(sprite, pos_screen_px, ori=0): # rotation_axis position inside o
 	pos = (pos[0] - axis_offset[0], pos[1] - axis_offset[1]) # correct for rotation_axis
 	
 	sprite.update(pos[0], pos[1], rotation)
+
+	sprite.opacity = alpha
+
 	sprite.draw()
 
 flappy_rotation_axis = (14,9) # px in sprite
@@ -75,35 +78,100 @@ wnd = pg.window.Window(width=background.width, height=background.height, caption
 
 ###########
 class Player:
+	class Input:
+		def __init__(self):
+			self.jump = False
+
 	class State(Enum):
 		playing = 0
 		crashing = 1
 		crashed = 2
+
+	def __init__(self, playing_flappy_x, initial_vel_y, input):
+		self.input = input
+
+		self.state = Player.State.playing
+
+		self.x = playing_flappy_x
+	
+		self.y = 100.0
+
+		self.vel_y = initial_vel_y
+
+		self.t_since_jump = 0
+
+		self.dist_score = 0
+		self.score = 0
+		
+	def score_point(self):
+		self.score += 1
+
+	def calc_ori(self):
+		return map_clamp(self.t_since_jump,  0.6, 1.0,  -8, 90)
+	def calc_flap_anim(self): # wing_ori
+		t = map(self.t_since_jump,  0.0, 0.13)
+		t = clamp(t)
+		
+		t = abs(-t +0.5) * 2
+		
+		return lerp(-60,0, t)
+
+human_input = Player.Input()
+
+class RandomJumper(Player.Input):
+	random_index = 0
+	
+	def __init__(self):
+		super(RandomJumper, self).__init__()
+
+		self.rand = random.Random(RandomJumper.random_index)
+
+		RandomJumper.random_index += 1
+
+		self.t_to_jump = self.rand.uniform(0.2, 0.8)
+
+	def update(self, dt):
+		self.t_to_jump -= dt
+		
+		if self.t_to_jump <= 0:
+			self.t_to_jump = self.rand.uniform(0.2, 0.8)
+
+			self.jump = True
 
 class Game:
 	def __init__(self):
 		self.reset()
 
 	def reset(self):
-		self.playing_flappy_x = 46 # where flappy is while player is playing (when player has )
-		self.flappy_x = self.playing_flappy_x
-
+		self.playing_flappy_x = 46 # where flappy is while player is playing (when player is crashing/crashed his x  )
+		
 		self.flappy_jump_vel_y = 120
+		
+		# Player creation
+		self.players = []
+		def new_player(*args, **kwargs):
+			pl = Player(self.playing_flappy_x, self.flappy_jump_vel_y / 2, *args, **kwargs)
+			self.players.append( pl )
+			return pl
 
-		self.flappy_y = 100.0
-		self.flappy_vel_y = self.flappy_jump_vel_y / 2
+		self.player_controlled = None
 
-		self.state = Player.State.playing
+		human_player = True
+		if human_player:
+			self.player_controlled = new_player(human_input)
 
-		self.flappy_t_since_jump = 0
+		for i in range(10):
+			ai = RandomJumper()
+
+			pl = new_player(ai)
+			pl.ai = ai
+
+		self.displayed_player = self.player_controlled if self.player_controlled else self.players[0]
+		#
 
 		self.grav_accel = -400
 
 		self.dist = 0
-
-		self.dist_score = 0
-		self.score = 0
-
 		self.speed = 70 # 70
 		
 		self.noise_seed = random.uniform(0,1000)
@@ -118,9 +186,6 @@ class Game:
 		self.pipes = deque()
 
 		self._dbg_t = 0
-
-	def score_point(self):
-		self.score += 1
 		
 	def need_to_gen_pipe_x(self):
 		
@@ -175,21 +240,22 @@ class Game:
 			self.pipes.append(self.Pipe(next_pipe_x, y, self.pipe_gap))
 
 			self.pipe_noise_x += self.pipes_seperation / self.screen_width
-			
-		if self.state == Player.State.playing:
-			for pipe in self.pipes:
-				if not pipe.passed and self.dist + self.flappy_x >= pipe.x + self.pipe_w / 2:
-					pipe.passed = True
-					self.score_point()
+		
+		for player in self.players:
+			if player.state == Player.State.playing:
+				for pipe in self.pipes:
+					if not pipe.passed and self.dist + player.x >= pipe.x + self.pipe_w / 2:
+						pipe.passed = True
+						player.score_point()
 	
-	def collide_flappy(self):
-		flappy_collider = collision.AAEllipse(pos=(self.flappy_x, self.flappy_y), size=(9 * flappy_body.scale_x / sprite_scale, 8.5 * flappy_body.scale_y / sprite_scale))
+	def collide_flappy(self, player):
+		flappy_collider = collision.AAEllipse(pos=(player.x, player.y), size=(9 * flappy_body.scale_x / sprite_scale, 8.5 * flappy_body.scale_y / sprite_scale))
 		
 		ground_collider = collision.AABB(min=(-math.inf, -math.inf), max=(+math.inf, +ground.height / sprite_scale))
 
 		colliding = collision.check(flappy_collider, ground_collider)
 		
-		if self.state == Player.State.playing:
+		if player.state == Player.State.playing:
 			for pipe in self.pipes:
 
 				x = pipe.x - self.dist
@@ -200,78 +266,74 @@ class Game:
 				colliding = colliding or collision.check(flappy_collider, pipe_top_collider)
 				colliding = colliding or collision.check(flappy_collider, pipe_bottom_collider)
 
-		if self.state == Player.State.playing and colliding:
-			self.state = Player.State.crashing
-			self.collided_dist = self.dist
-			self.flappy_vel_y = self.flappy_jump_vel_y * 0.3
+		if player.state == Player.State.playing and colliding:
+			player.state = Player.State.crashing
+			player.collided_dist = self.dist
+			player.vel_y = self.flappy_jump_vel_y * 0.3
 
-		elif self.state == Player.State.crashing and colliding:
-			self.state = Player.State.crashed
+		elif player.state == Player.State.crashing and colliding:
+			player.state = Player.State.crashed
 		
 		# prevent flappy from penetration ground
-		self.flappy_y = max(self.flappy_y, ground_collider.max[1] + flappy_collider.size[1])
+		player.y = max(player.y, ground_collider.max[1] + flappy_collider.size[1])
 
 		return colliding
 
 	def dbg_draw_colliders(self):
-		flappy_collider = collision.AAEllipse(pos=(self.flappy_x, self.flappy_y), size=(9 * flappy_body.scale_x / sprite_scale, 8.5 * flappy_body.scale_y / sprite_scale))
+		flappy_colliders = [collision.AAEllipse(pos=(player.x, player.y), size=(9 * flappy_body.scale_x / sprite_scale, 8.5 * flappy_body.scale_y / sprite_scale)) for player in self.players]
 		
 		ground_collider = collision.AABB(min=(-math.inf, -math.inf), max=(+math.inf, +ground.height / sprite_scale))
 
 		colliding = collision.check(flappy_collider, ground_collider)
 		
-		if self.state == Player.State.playing:
-			for pipe in self.pipes:
-				x = pipe.x - self.dist
+		#if self.state == Player.State.playing:
+		for pipe in self.pipes:
+			x = pipe.x - self.dist
 
-				draw_rect((x * sprite_scale, (pipe.y + pipe.gap/2) * sprite_scale), (self.pipe_w * sprite_scale, 9999 * sprite_scale))
-				draw_rect((x * sprite_scale, (pipe.y - pipe.gap/2 -9999) * sprite_scale), (self.pipe_w * sprite_scale, 9999 * sprite_scale))
+			draw_rect((x * sprite_scale, (pipe.y + pipe.gap/2) * sprite_scale), (self.pipe_w * sprite_scale, 9999 * sprite_scale))
+			draw_rect((x * sprite_scale, (pipe.y - pipe.gap/2 -9999) * sprite_scale), (self.pipe_w * sprite_scale, 9999 * sprite_scale))
 
 		draw_rect((-999 * sprite_scale, -999 * sprite_scale), ((999*2 +ground.width / sprite_scale) * sprite_scale, (ground.height / sprite_scale + 999) * sprite_scale))
 
-		draw_circle((flappy_collider.pos[0] * sprite_scale, flappy_collider.pos[1] * sprite_scale), (flappy_collider.size[0] * sprite_scale, flappy_collider.size[1] * sprite_scale), col = (255,255,40,180) if colliding else (40,255,40,180))
+		for flappy_collider in flappy_colliders:
+			draw_circle((flappy_collider.pos[0] * sprite_scale, flappy_collider.pos[1] * sprite_scale), (flappy_collider.size[0] * sprite_scale, flappy_collider.size[1] * sprite_scale), col = (255,255,40,180) if colliding else (40,255,40,180))
 
 	def update(self, dt):
 
-		self.dist += self.speed * dt# * (0.3 if self.state != Player.State.playing else 1)
+		self.dist += self.speed * dt
 
 		self.update_pipes()
 		
-		if self.state != Player.State.crashed:
-			self.flappy_vel_y += self.grav_accel * dt
+		for player in self.players:
+			if player.state != Player.State.crashed:
+				
+				if hasattr(player, "ai"):
+					player.ai.update(dt)
+				
+				if player.input.jump:
+					player.vel_y = self.flappy_jump_vel_y
 
-			self.flappy_vel_y = max(self.flappy_vel_y, -1500)
+					player.t_since_jump = 0
+				player.input.jump = False
 
-			self.flappy_y += self.flappy_vel_y * dt
-			self.flappy_y = clamp(self.flappy_y, -500, +1000)
+				player.vel_y += self.grav_accel * dt
 
-			self.collide_flappy()
+				player.vel_y = max(player.vel_y, -1500)
+
+				player.y += player.vel_y * dt
+				player.y = clamp(player.y, -500, +1000)
+
+				self.collide_flappy(player)
 		
-		if self.state != Player.State.playing:
-			self.flappy_x = self.playing_flappy_x + self.collided_dist - self.dist
+			if player.state != Player.State.playing:
+				player.x = self.playing_flappy_x + player.collided_dist - self.dist
 			
-		if self.state == Player.State.playing:
-			self.dist_score = self.dist
+			if player.state == Player.State.playing:
+				player.dist_score = self.dist
 
-		self.flappy_t_since_jump += dt
+			player.t_since_jump += dt
 		
 		self._dbg_t += dt
-
-	def jump(self):
-		if self.state == Player.State.playing:
-			self.flappy_vel_y = self.flappy_jump_vel_y
-
-			self.flappy_t_since_jump = 0
-
-	def calc_flappy_ori(self):
-		return map_clamp(self.flappy_t_since_jump,  0.6, 1.0,  -8, 90)
-	def calc_flappy_flap_anim(self): # wing_ori
-		t = map(self.flappy_t_since_jump,  0.0, 0.13)
-		t = clamp(t)
-		
-		t = abs(-t +0.5) * 2
-		
-		return lerp(-60,0, t)
 
 	def draw_pipes(self):
 		
@@ -288,12 +350,20 @@ class Game:
 		#
 		draw_sprite(background, (0,0))
 
-		#
-		flappy_ori = self.calc_flappy_ori()
-		flappy_wing_ori = self.calc_flappy_flap_anim()
+		# draw all players with weak alpha and displayed player with full alpha on top
+		def draw_player(player, alpha):
+			ori = player.calc_ori()
+			wing_ori = player.calc_flap_anim()
 
-		draw_sprite(flappy_body, (self.flappy_x,self.flappy_y), ori=flappy_ori) # rotate body and wings around same axis, so that they match up
-		draw_sprite(flappy_wings, (self.flappy_x,self.flappy_y), ori=flappy_ori + flappy_wing_ori)
+			draw_sprite(flappy_body, (player.x,player.y), ori=ori, alpha=alpha) # rotate body and wings around same axis, so that they match up
+			draw_sprite(flappy_wings, (player.x,player.y), ori=ori + wing_ori, alpha=alpha)
+
+		for player in self.players:
+			if player is not self.displayed_player:
+				draw_player(player, 80)
+		for player in self.players:
+			if player is self.displayed_player:
+				draw_player(player, 255)
 		
 		#
 		self.draw_pipes()
@@ -305,10 +375,10 @@ class Game:
 		draw_sprite(ground, (ground_x +self.screen_width, 0))
 
 		#
-		ldist.text = "dist: %4d" % self.dist_score
+		ldist.text = "dist: %4d" % self.displayed_player.dist_score
 		ldist.draw()
 		
-		lscore.text = "score: %3d" % self.score
+		lscore.text = "score: %3d" % self.displayed_player.score
 		lscore.draw()
 		
 		lrestart.draw()
@@ -332,11 +402,11 @@ def on_key_press(symbol, modifiers):
 	if symbol == key.R:
 		game.reset()
 	else:
-		game.jump()
+		human_input.jump = True
 		
 @wnd.event
 def on_mouse_press(x, y, button, modifiers):
-    game.jump()
+	human_input.jump = True
 
 pg.clock.schedule_interval(update, 1 / 60)
 
